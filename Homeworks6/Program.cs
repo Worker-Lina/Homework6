75594488731b364cdd480598fdbb8dc2425cd8de
алина человечкова <alinasmart19@mail.ru>
2020-12-27 20:25:22 +0600

    Добавьте файлы проекта.

---
+++ Homeworks6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using MyClassLib.WordOfTanks;
+
+namespace Homeworks6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int countOfTanks = 5;
+            var t34 = new Tank[countOfTanks];
+            var pantera = new Tank[countOfTanks];
+
+            try
+            {
+
+                for (int i = 0; i < t34.Length; i++)
+                {
+                    t34[i] = new Tank("T34", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+                    pantera[i] = new Tank("Pantera", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+            try
+            {
+                for (int i = 0; i < countOfTanks; i++)
+                {
+                    Console.WriteLine($"\tБоекомплект | Броня | Маневренность\n" +
+                        $" T34\t{t34[i].GetAllowanceOfAmmunition()}\t    | {t34[i].GetArmor()}    | {t34[i].GetManeuverability()}\n" +
+                        $" Pant\t{pantera[i].GetAllowanceOfAmmunition()}\t    | { pantera[i].GetArmor()}    | { pantera[i].GetManeuverability()}");
+
+                    string winner = t34[i] * pantera[i];
+                    if (winner == "ничья")
+                    {
+                        Console.WriteLine("Ничья");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nВ {i + 1} схватке победил танк {winner}");
+                    }
+                    Console.WriteLine("-------------------------------------------");
+                    Console.WriteLine();
+                }
+            }catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+           
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,57 @@
+﻿namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        private string name;
+        private int allowanceOfAmmunition;
+        private int armor;
+        private int maneuverability;
+
+
+        public Tank() { }
+
+        public Tank(string name, int allowanceOfAmmunition, int armor, int maneuverability)
+        {
+            this.name = name;
+            this.allowanceOfAmmunition = allowanceOfAmmunition;
+            this.armor = armor;
+            this.maneuverability = maneuverability;
+        }
+
+        
+        public string GetAllowanceOfAmmunition()
+        {
+            return allowanceOfAmmunition.ToString(); 
+        }
+
+        public string GetArmor()
+        {
+            return armor.ToString();
+        }
+
+        public string GetManeuverability()
+        {
+            return maneuverability.ToString();
+        }
+
+        public static string operator *(Tank T34, Tank pantera)
+        {
+            if (T34.allowanceOfAmmunition > pantera.allowanceOfAmmunition && T34.armor > pantera.armor ||
+                T34.allowanceOfAmmunition > pantera.allowanceOfAmmunition && T34.maneuverability > pantera.maneuverability ||
+                T34.armor > pantera.armor && T34.maneuverability > pantera.maneuverability)
+            {
+                return T34.name;
+            }
+            else if (T34.allowanceOfAmmunition < pantera.allowanceOfAmmunition && T34.armor < pantera.armor ||
+                     T34.allowanceOfAmmunition < pantera.allowanceOfAmmunition && T34.maneuverability < pantera.maneuverability ||
+                     T34.armor < pantera.armor && T34.maneuverability < pantera.maneuverability)
+            {
+                return pantera.name;
+            }
+            else
+            {
+                return "Ничья";
+            }
+        }
+    }
+}
